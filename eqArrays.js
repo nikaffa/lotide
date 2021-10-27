e557897a2bb116f373941776e3e0b1d87d2ddf70
Veronika Fatikhova <fidelio@list.ru>
2021-10-27 14:28:37 -0700

    eqArrays.js added

---
+++ eqArrays.js
@@ -0,0 +1,23 @@
+//Takes in two arrays and returns true or false, based on a perfect match.
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Test function: compares two values and prints out a message telling if they match or not.
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 1]), true);
