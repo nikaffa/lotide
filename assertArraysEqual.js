be0964dd5c6a1595c5adcb2b33a0fdd49102e3a0
Veronika Fatikhova <fidelio@list.ru>
2021-10-27 14:31:02 -0700

    assetArraysEqual.js added

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+//Takes in two arrays and returns true or false, based on a perfect match.
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+//Test function: take in two arrays and console.log an appropriate message
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+
