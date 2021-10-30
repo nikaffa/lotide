caba822ab258ec718874f7c7b2274ec6bcec439f
Veronika Fatikhova <fidelio@list.ru>
2021-10-30 13:43:21 -0700

    findByValue added

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+// Given an object and a string, returns a key which value matched the string
+const findKeyByValue = (object, value) => {
+  const arr = Object.keys(object);
+  for (let item of arr) {
+    if (object[item] === value) {
+      return item;
+    }
+  }
+};
+
+// Test function: compares two values on equality
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Test case:
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+
+
