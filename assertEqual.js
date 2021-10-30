83a6d831a86d3ac017dcbe8cae1320fbaf72cc76
Veronika Fatikhova <fidelio@list.ru>
2021-10-30 13:12:42 -0700

    assertEqual modified

--- assertEqual.js
+++ assertEqual.js
@@ -1,5 +1,5 @@
 // Compares two values on equality
-const assertEqual = function(actual, expected) {
+const assertEqual = (actual, expected) => {
   if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
