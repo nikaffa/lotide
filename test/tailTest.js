810c179dc35ad4e2b42df139a0c8a5639408104a
Veronika Fatikhova <fidelio@list.ru>
2021-11-01 17:36:01 -0700

    tailTest added

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,14 @@
+const assert = require('chai').assert;
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
 
-// Test Case 1:
-let result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-assertEqual(result[2], undefined);
-
-// Test Case 2:
-result = tail(["Hello"]);
-assertEqual(result.length, 0);
-assertEqual(result[1], undefined);
-
-// Test Case 3:
-result = tail([]);
-assertEqual(result.length, 0);
-assertEqual(result[0], undefined);
+describe("#tail", () => {
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it("returns [] for ['Hello']", () => {
+    assert.deepEqual(tail(['Hello']), []);
+  });
+  it("returns  ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ["Lighthouse", "Labs"]);
+  });
+});
